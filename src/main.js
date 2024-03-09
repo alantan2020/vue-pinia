3e5ec5c1433d8ae63bb5daba29ca9c885f7fb85e
alantan2020 <582704597@qq.com>
2024-03-09 18:24:56 +0800

    把pinia加到app中

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia"; //1.导入createPinia
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const pinia = createPinia(); //2.执行createPinia 得到一个pinia的实例
+const app = createApp(App);
+
+app.use(pinia); //3.把这个pinia的实例加入到app中
+app.mount("#app");
+
+// createApp(App).mount('#app')
